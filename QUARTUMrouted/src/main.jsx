bcb3516bbd67f3f91f66cbd4842ea9e6b0f0ead9
aasspaassp <andresfilos94@gmail.com>
2023-10-09 23:28:10 -0600

    router and layout

--- QUARTUMrouted/src/main.jsx
+++ QUARTUMrouted/src/main.jsx
@@ -1,34 +1,54 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Root,  { loader as rootLoader, action as rootAction } from './routes/root'
+import Root, { loader as rootLoader, action as rootAction } from './routes/root'
 import ErrorPage from './error-page'
 import Propiedad, {
   loader as porpiedadLoader,
 } from "./routes/propiedad";
+import Contacto from './routes/contacto'
+import Legal from './routes/legal'
+import Nav from './components/navbar'
 import {
   createBrowserRouter,
   RouterProvider,
+  Link,
+  Outlet,
 } from "react-router-dom";
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Root />,
+    // refactor
+    Component: Nav,
     errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
     children: [
       {
-        path: "propiedades/:propiedadId",
+        index: true,
+        path: "propiedades",
+        element: <Root />,
+        loader: rootLoader,
+        action: rootAction,
+      },
+      {
+        path: ":propiedadId",
         element: <Propiedad />,
         loader: porpiedadLoader,
       },
-    ]
-  },
+      {
+        path: "contacto",
+        element: <Contacto/> ,
+      },
+      {
+        path: "legal",
+        element: <Legal/>,
 
+      },
+    ]
+  }
 ])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
