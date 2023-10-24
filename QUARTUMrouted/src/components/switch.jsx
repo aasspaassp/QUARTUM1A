1f6ae1dbd1a301616457bbc045d7b725456c5719
aasspaassp <andresfilos94@gmail.com>
2023-10-24 11:51:25 -0600

    build

--- QUARTUMrouted/src/components/switch.jsx
+++ QUARTUMrouted/src/components/switch.jsx
@@ -11,50 +11,50 @@ export default function Switch() {
     return (
         <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
             <div className="flex flex-1 justify-between sm:hidden">
-                <Link
+                <button
                     to="/venta"
                     className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
                 >
                     Venta
-                </Link>
-                <Link
-                    href="/"
+                </button>
+                <button
+                    to="/"
                     className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
                 >
                     Todo
-                </Link>
-                <Link
-                    href="/renta"
+                </button>
+                <button
+                    to="/renta"
                     className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
                 >
                     Renta
-                </Link>
+                </button>
             </div>
             <div className="hidden sm:flex sm:flex-1 sm:items-center ">
                 <div>
                     <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-                        <Link
-                            href="/"
+                        <button
+                            to="/"
                             className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
                             Todo
 
-                        </Link>
+                        </button>
                         {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-                        <Link
-                            href="/venta"
+                        <button
+                            to="/venta"
             
                             className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
                             Venta
-                        </Link>
-                        <Link
-                            href="/renta"
+                        </button>
+                        <button
+                            to="/renta"
                             className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
                             Renta
 
-                        </Link>
+                        </button>
                     </nav>
                 </div>
             </div>
--- QUARTUMrouted/src/main.jsx
+++ QUARTUMrouted/src/main.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import './index.css'
+
 import Root, { loader as rootLoader, action as rootAction } from './routes/root'
+
 import ErrorPage from './error-page'
+
 import Propiedad, {
   loader as porpiedadLoader,
 } from "./routes/propiedad";
+
 import Contacto from './routes/contacto'
 import Legal from './routes/legal'
 import Nav from './components/navbar'
+
 import {
   createBrowserRouter,
   RouterProvider,
-  Link,
-  Outlet,
-  redirect,
 } from "react-router-dom";
 
 
@@ -31,40 +34,6 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
 
-  },
-  {
-    path: "/venta",
-    element: <Root/>,
-    loader: async () => {
-      const response = await fetch(`/propiedades/filtro/venta`);
-
-      if (!response.ok) {
-        const message = `An error occurred: ${response.statusText}`;
-        window.alert(message);
-        return;
-      }
-    
-      const propiedades = await response.json();
-      console.log('respuesta servidor', propiedades)
-      return { propiedades }
-    }  
-  },
-  {
-    path: "/renta",
-    element: <Root/>,
-    loader: async () => {
-      const response = await fetch(`/propiedades/filtro/renta`);
-
-      if (!response.ok) {
-        const message = `An error occurred: ${response.statusText}`;
-        window.alert(message);
-        return;
-      }
-    
-      const propiedades = await response.json();
-      console.log('respuesta servidor', propiedades)
-      return { propiedades }
-    }  
   },
   {
     path: "propiedades/:propiedadId",
